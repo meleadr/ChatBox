2236ba91877581b9d4364a68d862a9f83090714a
meleadr <meledina.adriano@gmail.com>
2023-05-23 19:27:29 +0200

    Mise en place du systeme de login

---
+++ api.php
@@ -0,0 +1,49 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Define configuration here
+$dbhost = 'localhost';
+$dbname = 'chatbox';
+$dbuser = 'root';
+$dbpass = 'root';
+
+try {
+    // Create a new PDO instance
+    $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    // If there is an error in the connection, stop the script and display the error.
+    die("Failed to connect to the database: " . $e->getMessage());
+}
+
+// Get JSON as a string
+$json_str = file_get_contents('php://input');
+
+// Get as an object
+$json_obj = json_decode($json_str);
+
+$email = $json_obj->email;
+$password = $json_obj->password;
+
+$stmt = $db->prepare("SELECT * FROM users WHERE mail = :email");
+$stmt->bindParam(':email', $email);
+
+$stmt->execute();
+
+if($stmt->rowCount() > 0){
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if(password_verify($password, $user['password'])){
+        // Correct Password
+        echo json_encode(['status' => 'success', 'message' => 'Login successful']);
+    }else{
+        // Wrong Password
+        echo json_encode(['status' => 'error', 'message' => 'Wrong password']);
+    }
+}else{
+    // Email not registered
+    echo json_encode(['status' => 'error', 'message' => 'Email not registered']);
+}
+
+?>
--- main.js
+++ main.js
@@ -1,24 +1,60 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector("#app");
+
+app.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <h1>Connexion</h1>
+	<div class="card">
+		<form id="login-form">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email" placeholder="Email">
+			<label for="password">Mot de passe</label>
+			<input type="password" id="password" name="password" placeholder="Mot de passe">
+			<button type="submit">Se connecter</button>
+		</form>
+	</div>
+</div>
+`;
 
-setupCounter(document.querySelector('#counter'))
+const form = document.querySelector("#login-form");
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const formData = new FormData(form);
+	const data = Object.fromEntries(formData);
+	fetch("http://localhost:8888/api.php", {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			// handle the response data
+			console.log(data);
+
+			// clear everything within #app
+			app.innerHTML = "";
+
+			// add chatbox HTML structure here
+			app.innerHTML = `
+		  <div>
+			<h1>Chatbox</h1>
+			<div class="chatbox">
+				<!-- Add chatbox structure here -->
+			</div>
+		  </div>
+		`;
+		})
+		.catch((error) => {
+			// handle the error
+			console.error("Error:", error);
+			// You might want to display an error message to the user here
+		});
+});
